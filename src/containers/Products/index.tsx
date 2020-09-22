53e3c90a7dc6a8e1b6cbeda50b0107c077e4b276
Fernando Ramos <ferramgon1@gmail.com>
2020-09-22 23:55:14 +0200

    creates empty containers

---
+++ src/containers/Products/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Products = () => (
+  <div>Product List</div>
+);
+
+export default Products
---
+++ src/containers/ShoppingCart/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ShoppingCart = () => (
+  <div>ShoppingCart</div>
+);
+
+export default ShoppingCart
